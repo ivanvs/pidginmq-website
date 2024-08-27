4a2a42ea7e19fcbdae55bcf1e83e19982683f2ce
ivanvs <robince885@gmail.com>
2024-08-27 20:51:38 +0200

    Remove blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,6 @@ const config: Config = {
           position: "left",
           label: "Docs",
         },
-        { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/ivanvs/pidginmq",
           label: "GitHub",
@@ -107,10 +106,6 @@ const config: Config = {
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
             {
               label: "GitHub",
               href: "https://github.com/ivanvs/pidginmq",
